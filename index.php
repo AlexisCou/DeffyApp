236ac30cd8fffd899833d12af3e363acf061bd9a
AlexisCou <alexis.couturier9@etu.univ-lorraine.fr>
2025-10-08 15:23:00 +0200

    Fin de l'exo1 et 2

--- index.php
+++ index.php
@@ -1,19 +1,6 @@
 <?php
-session_start();
-
 require_once 'vendor/autoload.php';
-require_once 'action/Action.php';
-require_once 'action/DefaultAction.php';
-require_once 'action/DisplayPlaylistAction.php';
-require_once 'action/AddPlaylistAction.php';
-require_once 'action/AddPodcastTrackAction.php';
-require_once 'classes/AudioList.php';
-require_once 'classes/Playlist.php';
-require_once 'classes/AudioTrack.php';
-require_once 'classes/AlbumTrack.php';
-require_once 'classes/PodcastTrack.php';
-require_once 'classes/AudioListRenderer.php';
-require_once 'dispatch/Dispatcher.php';
+session_start();
 
 use iutnc\deefy\dispatch\Dispatcher;
 
